80e8b43ab7642497525edda9b280d1ae8f45b1b3
wakasso <wakasso42furtif@gmail.com>
2022-03-11 14:41:40 -0800

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ * Description: prints all possible different combinations of two digits.
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
