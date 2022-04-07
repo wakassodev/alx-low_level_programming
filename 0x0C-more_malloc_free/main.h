4746dbbdd655ebe9ef071e4409ab1fe3ce6a41bf
wakasso <wakasso42furtif@gmail.com>
2022-04-08 00:22:26 +0400

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define ALX_H
 
 int onlyNumbers(char *c);
-void multiply(char* s1, char* s2);
+void multiply(char* num1, char* num2);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -10,6 +10,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *);
-void _is_zero(char *argv[]);
-char	*_initialize_array(char *ar, int lar);
 #endif
