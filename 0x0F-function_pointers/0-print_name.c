4b50f445471e7003fe5a011b4e312b46eb3962bb
wakasso <wakasso42furtif@gmail.com>
2022-04-14 00:37:46 +0400

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-print_name.c
+ * Author: Idi Wakasso
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: callback function that prints name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,6 @@
+|   ALX SE   |  0x0B-malloc_free  |
+| :--------: | -------- |
+| 0x0F. C - Function pointers     | Life in Coding      |
+  ![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+| :--------: | -------- |
+|Comment      | Like :)     |
